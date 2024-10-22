24df34e1f39934c50e45c889b2fd37b9803a2be3
Eren Cinar <erencnnr06@gmail.com>
2024-10-22 21:54:34 +0300

    u

--- App.js
+++ App.js
@@ -3,18 +3,21 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import StartScreen from './components/StartScreen';
 import Gallery from './components/Gallery';
-
+import RealmProvider from './database/RealmProvider';
+import { registerRootComponent } from 'expo';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
+    <RealmProvider>
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start" screenOptions={{headerShown : false}}>
         <Stack.Screen name="Start" component={StartScreen} />
         <Stack.Screen name="Gallery" component={Gallery} />
       </Stack.Navigator>
     </NavigationContainer>
+    </RealmProvider>
   );
 }
-
+registerRootComponent(App)
 
--- database/RealmProvider.js
+++ database/RealmProvider.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import Realm from "realm";
+//import Realm from "realm";
 
 export default function RealmProvider({children}) {
   return (
