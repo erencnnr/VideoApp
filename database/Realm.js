809c67c299e293283a23534bd40718c128a5a78e
Eren Cinar <erencnnr06@gmail.com>
2024-10-22 20:41:11 +0300

    realm2

---
+++ database/Realm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Realm from "realm";
+
+export default function RealmProvider({children}) {
+  return (
+    <>{children}</>
+  );
+};
+
